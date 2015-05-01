633ca6114f71836939500e22f4fa163a9f369593
Taras Rodionov <moonrtv@ya.ru>
2015-05-01 15:56:12 +0400

    Push

--- js/map.js
+++ js/map.js
@@ -1,14 +1,14 @@
 function initialize() {
   var mapOptions = {
           zoom: 10,
+          scrollwheel: false,
           center: new google.maps.LatLng(34.8491662,-111.7484466)
   }
   var map = new google.maps.Map(document.getElementById('map-canvas'),mapOptions);
   var myLatLng = new google.maps.LatLng(34.8491662,-111.7484466);
   var beachMarker = new google.maps.Marker({
     position: myLatLng,
-    map: map,
-    icon: image
+    map: map
   });
 }
 google.maps.event.addDomListener(window, 'load', initialize);
