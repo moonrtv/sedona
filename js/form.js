63c39a5b311c078ab650e568c5ba5e98cd3f515d
Rodionov Taras <moonrtv@ya.ru>
2015-10-17 21:29:39 +0300

    Start writing js

--- js/form.js
+++ js/form.js
@@ -1,5 +1,32 @@
 var btnSearch = document.querySelector(".btn");
+var btnForm = document.querySelector(".btn-form");
+var btnFormMinusPlus = document.querySelector(".block");
 var popup = document.querySelector(".modal-content");
+
+btnFormMinusPlus.onclick = function(event) {
+  event.preventDefault();
+  var target = event.target,
+      btnMinus = target.closest(".icon-minus"),
+      btnPlus = target.closest(".icon-plus");
+
+  if ((!btnMinus || btnPlus) && (btnMinus || !btnPlus)) return;
+  searchInput(target);
+};
+
+function searchInput(node) {
+  var parentNode = node.parentNode.parentNode,
+      childNode = parentNode.querySelector("input");
+  if (childNode) setNumber(childNode);
+}
+
+function setNumber(node) {
+  console.log('Set number');
+}
+
+btnForm.addEventListener("click", function (event) {
+      event.preventDefault();
+      window.alert(document.cookie);
+});
 
 btnSearch.addEventListener("click", function (event) {
 			event.preventDefault();
